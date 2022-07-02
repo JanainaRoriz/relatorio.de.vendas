e325b3490ceea29e5848cf01aa5753a6638b2d77
JanainaRoriz <janainalimar@gmail.com>
2022-07-02 17:39:29 -0300

    Acrescentei o método groupby para filtrar as colunas e os valores que quero no relatório

--- relatorio.de.vendas.py
+++ relatorio.de.vendas.py
@@ -2,3 +2,15 @@ import pandas as pd
 
 tabela_vendas = pd.read_excel('vendas.xlsx')
 print(tabela_vendas)
+
+#faturamento de cada loja
+faturamento = tabela_vendas[['ID Loja', 'Valor final']].groupby('ID Loja').sum()
+print(faturamento)
+
+#quantidade de produtos vendidos em cada loja
+num_vendas = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+print(num_vendas)
+
+#ticket médio por produto de cada loja
+
+#enviar um email com o relatório de vendas
