c869e638037766b71cd4e636faa06ce9d1fd6828
JanainaRoriz <janainalimar@gmail.com>
2022-07-05 00:42:05 -0300

    codigo de envio de email adicionado

--- relatorio.de.vendas.py
+++ relatorio.de.vendas.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import smtplib
+import ssl
 
 tabela_vendas = pd.read_excel('vendas.xlsx')
 print(tabela_vendas)
@@ -16,3 +18,28 @@ ticket_medio = faturamento['Valor final'] / num_vendas['Quantidade'].to_frame()
 print(ticket_medio)
 
 #enviar um email com o relatório de vendas
+port = 587
+
+smtp_remetente = "smtp-mail.outlook.com"
+
+remetente = "teste.codigopy@outlook.com"
+
+destinatario = "loja.amazing.things@gmail.com"
+
+senha_remetente = "codigopy.1"
+
+mensagem = """
+
+Subject: mensagem teste
+
+"""
+
+SSL_context = ssl.create_default_context()
+
+with smtplib.SMTP(smtp_remetente, port) as server:
+
+    server.starttls(context=SSL_context)
+
+    server.login(remetente,senha_remetente)
+
+    server.sendmail(remetente, destinatario, mensagem)
